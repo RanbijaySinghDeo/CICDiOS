9da7f6e1018a9b130d8ae531bd7f4b2f51739e8e
Ranbijay <ranbijaysinghdeo@gmail.com>
2026-07-02 19:55:58 +0530

    Initial Commit

---
+++ RetirementCalculator/RetirementCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetirementCalculatorApp.swift
+//  RetirementCalculator
+//
+//  Created by Ranbijay SinghDeo on 02/07/26.
+//
+
+import SwiftUI
+
+@main
+struct RetirementCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
